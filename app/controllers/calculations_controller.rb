ef48c8fdc88ae0efb467ab7a57896bac14003ff2
Danielle Calderone <danier29@users.noreply.github.com>
2017-04-24 23:36:04 -0500

    updated 1136pm

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -22,23 +22,69 @@ end
 def flexible_payment
 
 #:apr/:years/:principal
-  @user_apr=params["apr"].to_f
-  @user_years=params["year"].to_f
+  @user_apr=(params["apr"].to_f)/100
+  interest_rate_per_month= @user_apr/(12*100)
+  @user_years=params["years"].to_f
   @user_principal=params["principal"].to_f
 
+
+  numerator= interest_rate_per_month
+  @total_months= @user_years*12
+  denom_pta= (1+(interest_rate_per_month))**(-1*@total_months)
+  denominator= 1- denom_pta
+
+  @user_monthly_payment= (numerator/denominator)*@user_principal
+
   render("calculations/flexible_payment.html.erb")
+
 end
 
+#/flexible/random/:beg_number/:end_number"
+def flexible_random
+@beg_number=(params["beg_number"].to_f)
+@end_number=(params["end_number"].to_f)
+@random_gen_number= rand(@beg_number..@end_number)
+render("calculations/flexible_random.html.erb")
+
+end
 
 def square
 @user_num= params[:user_num].to_f
   render("calculations/square.html.erb")
 end
 
-
 def square_form
-
+  render("calculations/square_form.html.erb")
 end
 
+def square_root
+  @user_num= params[:user_num].to_f
+  render("calculations/square_root.html.erb")
+end
+
+def square_root_form
+  render("calculations/square_root_form.html.erb")
+  end
+
+def payment_form
+  render("calculations/payment_form.html.erb")
+  #code
+end
+
+def payment
+  @user_apr=(params["apr"].to_f)/100
+  interest_rate_per_month= @user_apr/(12*100)
+  @user_years=params["years"].to_f
+  @user_principal=params["principal"].to_f
+
+  numerator= interest_rate_per_month
+  @total_months= @user_years*12
+  denom_pta= (1+(interest_rate_per_month))**(-1*@total_months)
+  denominator= 1- denom_pta
+
+  @user_monthly_payment= (numerator/denominator)*@user_principal
+
+  render("calculations/payment.html.erb")
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 get("/flexible/square/:number", {:controller => "calculations", :action => "flexible_square_5"})
 get("/flexible/square_root/:number", {:controller => "calculations", :action => "flexible_square_root"})
 get("/flexible/payment/:apr/:years/:principal", {:controller => "calculations", :action => "flexible_payment"})
+get("/flexible/random/:beg_number/:end_number", {:controller => "calculations", :action => "flexible_random"})
 get("/square/new", {:controller => "calculations", :action => "square_form"})
 get("/square/results", {:controller => "calculations", :action => "square"})
-
+get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+get("/square_root/results", {:controller => "calculations", :action => "square_root"})
+get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+get("/payment/results", {:controller => "calculations", :action => "payment"})
 end
